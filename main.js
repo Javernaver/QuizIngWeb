7ef10f1ef90ab2d755a2c1163c6bfb4b45b93c36
Javernaver <Javernaver@gmail.com>
2020-10-21 17:18:27 -0300

    v0.2

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 "use strict";
 var Quiz = /** @class */ (function () {
     function Quiz() {
-        var men = document.getElementById("menu");
-        men.addEventListener("click", function (e) {
-            var esCat = e.target.nodeName === 'A';
+        var menu = document.getElementById("menu"); // leer el menu con todas las categorias
+        menu.addEventListener("click", function (e) {
+            var esCat = e.target.nodeName === 'A'; // comprobar si el elemento es del tipo "A" para las categorias del menu
             if (!esCat) {
                 return;
             }
-            document.getElementById("ubicacion").innerHTML = e.target.getAttribute('value');
-            console.log(e.target.getAttribute('value'));
+            document.getElementById("ubicacion").innerHTML = e.target.getAttribute('value'); // escribir el valor de value del elemento seleccionado en el elemento ubicacion
+            console.log(e.target.getAttribute('value')); // escribir en consola el value del elemento seleccionado
         });
     }
     return Quiz;
--- main.ts
+++ main.ts
@@ -4,16 +4,16 @@ class Quiz{
 
     constructor(){
 
-        let men = document.getElementById("menu");
+        let menu = document.getElementById("menu"); // leer el menu con todas las categorias
         
-        men.addEventListener("click", (e:Event) => {
-            let esCat = e.target.nodeName === 'A';
+        menu.addEventListener("click", (e:Event) => { // listerner para cuando se hace click en algun elemento del menu
+            let esCat = e.target.nodeName === 'A'; // comprobar si el elemento es del tipo "A" para las categorias del menu
               if (!esCat) {
                 return;
               }
 
-              document.getElementById("ubicacion").innerHTML = (<HTMLInputElement>e.target).getAttribute('value');
-              console.log((<HTMLInputElement>e.target).getAttribute('value'));
+              document.getElementById("ubicacion").innerHTML = (<HTMLInputElement>e.target).getAttribute('value'); // escribir el valor de value del elemento seleccionado en el elemento ubicacion
+              console.log((<HTMLInputElement>e.target).getAttribute('value')); // escribir en consola el value del elemento seleccionado
 
         });
         
